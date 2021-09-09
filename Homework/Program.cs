ce0b34348f77a6f2bb6895b9072c016b9ff59eda
FocusLaptop <maniaclord@bk.ru>
2021-09-09 16:30:38 +0300

    YaMolodec

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,7 +6,22 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var val1 = int.Parse(args[0]);
+            var operation = args[1];
+            var val2 = int.Parse(args[2]);
+
+            var result = 0;
+            switch (operation)
+            {
+                case "+": result = val1 + val2; break;
+                case "-": result = val1 - val2; break;
+                case "*": result = val1 * val2; break;
+                case "/": result = val1 / val2; break;
+            };
+
+
+
+            Console.WriteLine($"Result = {result}");
 
         }
     }
