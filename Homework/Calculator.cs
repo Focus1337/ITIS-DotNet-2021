cc186de17e78a662181ebbaf96d9236f0ef9697b
Focus <maniaclord@bk.ru>
2021-09-15 17:45:34 +0300

    2k-164 CoverageFix

--- Homework/Calculator.cs
+++ Homework/Calculator.cs
@@ -10,16 +10,41 @@
             Multiply
         }
 
-        public static int Calculate(int val1, Operation operation, int val2)
+        // public static int Calculate(int val1, Operation operation, int val2)
+        // {
+        //     return operation switch
+        //     {
+        //         Operation.Plus => val1 + val2,
+        //         Operation.Minus => val1 - val2,
+        //         Operation.Multiply => val1 * val2,
+        //         Operation.Divide => val1 / val2,
+        //         _ => throw new System.NotImplementedException()
+        //     };
+        // }
+        
+        public static bool Calculate(int val1, Operation operation, int val2, out int result)
         {
-            return operation switch
+            result = 0;
+            switch (operation)
             {
-                Operation.Plus => val1 + val2,
-                Operation.Minus => val1 - val2,
-                Operation.Multiply => val1 * val2,
-                Operation.Divide => val1 / val2,
-                _ => throw new System.NotImplementedException()
-            };
+                case Calculator.Operation.Plus:
+                    result = val1 + val2;
+                    break;
+                case Calculator.Operation.Minus:
+                    result = val1 - val2;
+                    break;
+                case Calculator.Operation.Multiply:
+                    result = val1 * val2;
+                    break;
+                case Calculator.Operation.Divide:
+                    if (val2 == 0)
+                    {
+                        return true;
+                    }
+                    result = val1 / val2;
+                    break;
+            }
+            return false;
         }
     }
 }
