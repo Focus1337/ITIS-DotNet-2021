bb4154364569235e4b1c070bde9ee5599e5734b1
Focus <maniaclord@bk.ru>
2021-09-22 21:03:41 +0300

    IL Parser Release

--- Homework/Parser.cs
+++ Homework/Parser.cs
@@ -5,44 +5,37 @@ namespace Calculator
     {
         public static bool TryParseOperatorOrQuit(string arg, out Calculator.Operation operation)
         {
-            switch (arg)
+            if (arg == "+")
+                operation = Calculator.Operation.Plus;
+            else if (arg == "-")
+                operation = Calculator.Operation.Minus;
+            else if (arg == "*")
+                operation = Calculator.Operation.Multiply;
+            else if (arg == "/")
+                operation = Calculator.Operation.Divide;
+            else
             {
-                case "+":
-                    operation = Calculator.Operation.Plus;
-                    break;
-                case "-":
-                    operation = Calculator.Operation.Minus;
-                    break;
-                case "*":
-                    operation = Calculator.Operation.Multiply;
-                    break;
-                case "/":
-                    operation = Calculator.Operation.Divide;
-                    break;
-                default:
-                    operation = default;
-                    return true;
-            };
-            return false;
-        }
-        public static bool TryParseArgsOrQuit(string arg, out int result)
-        {
-            if (!int.TryParse(arg, out result))
-            {
-                Console.WriteLine($"Value isn't int: {arg}");
+                operation = default;
                 return true;
             }
+
             return false;
+        }
+
+        public static bool TryParseArgsOrQuit(string arg, out int result)
+        {
+            if (int.TryParse(arg, out result))
+                return false;
+            Console.WriteLine($"Value isn't int: {arg}");
+            return true;
         }
 
         public static bool CheckArgsLengthOrQuit(string[] args)
         {
-            if (args.Length != 3)
-            {
-                Console.WriteLine($"The program requires 3 CLI arguments, but {args.Length} provided");
-                return true;
-            }
-            return false;
+            if (args.Length == 3)
+                return false;
+            Console.WriteLine($"The program requires 3 CLI arguments, but {args.Length} provided");
+            return true;
         }
     }
 }
