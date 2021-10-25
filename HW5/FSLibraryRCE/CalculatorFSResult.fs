74856ffbfcb8ccf168f81aeddd2c4371cd4b0880
FocusLaptop <maniaclord@bk.ru>
2021-10-25 12:27:58 +0300

    temp

--- HW5/FSLibraryRCE/CalculatorFSResult.fs
+++ HW5/FSLibraryRCE/CalculatorFSResult.fs
@@ -1,2 +1,57 @@
-﻿module FSLibraryRCE.CalculatorFSResult
+﻿namespace FSLibraryRCE
 
+open System
+
+type ResultBuilder(errorMessage: string) =
+    member b.Zero() = Error errorMessage
+
+    member b.Bind(x, f) =
+        match x with
+        | Ok x -> f x
+        | Error _ -> x
+
+    member b.Return x = Ok x
+    member b.Combine(x, f) = f x
+
+
+module CalculatorFS =
+    let private defaultResult = ResultBuilder("unknown error")
+
+    //let devByZero = "val2 was 0"
+    
+    let NotEnoughArgs = Exception("Not enough args")
+    
+    let WrongArgFormat = Exception("Wrong arg format")
+     
+    let WrongOperation = Exception("Wrong operation")
+     
+    let AttemptToDivideByZero = DivideByZeroException("Attempt to divide by zero")
+    
+    let OutOfRange = ArgumentOutOfRangeException("Argument out of range")
+
+    type Operation =
+        | Plus
+        | Minus
+        | Divide
+        | Multiply
+
+    let inline calculate
+        (val1: Result<'T, string> when 'T: (static member (+) : 'T * 'T -> 'T) and 'T: (static member (-) :
+                   'T * 'T -> 'T) and 'T: (static member (*) : 'T * 'T -> 'T) and 'T: (static member (/) : 'T * 'T -> 'T))
+        (val2: Result<'T, string>)
+        (operation: Result<Operation, string>)
+        =
+        match operation with
+        | Ok operation ->
+            ResultBuilder(AttemptToDivideByZero.Message) {
+                let! val11 = val1
+                let! val22 = val2
+                match operation with
+                | Plus -> return val11 + val22
+                | Divide ->
+                    if val22 <> new 'T() then
+                        return val11 / val22
+                | Minus -> return val11 - val22
+                | Multiply -> return val11 * val22
+            }
+        | Error operationError -> Error operationError
--- HW5/FSLibraryRCE/ParserFSResult.fs
+++ HW5/FSLibraryRCE/ParserFSResult.fs
@@ -1,2 +1,45 @@
-﻿module FSLibraryRCE.ParserFSResult
+﻿namespace FSLibraryRCE
 
+open System
+open FSLibraryRCE.CalculatorFS
+
+module ParserFs =
+    let wrongOperation = "Wrong operation"
+
+    let parseCalculatorOperation arg =
+        ResultBuilder(wrongOperation) {
+            if arg = "+" || arg = "-" || arg = "*" || arg = "/" then
+                match arg with
+                | "+" -> return Operation.Plus
+                | "-" -> return Operation.Minus
+                | "*" -> return Operation.Multiply
+                | _ -> return Operation.Divide
+        }
+
+    let numberErrorMessage = "value is not int"
+    let private resultNumber = ResultBuilder(numberErrorMessage)
+
+    //надо эти 4 метода собрать в 1, вызывать в них T.TryParse. хз как это делать
+    let parseInt (str: string) =
+        resultNumber {
+            let success, result = Int32.TryParse str
+            if success then return result
+        }
+
+    let parseDouble (str: string) =
+        resultNumber {
+            let success, result = Double.TryParse str
+            if success then return result
+        }
+
+    let parseFloat (str: string) =
+        resultNumber {
+            let success, result = Single.TryParse str
+            if success then return result
+        }
+
+    let parseDecimal (str: string) =
+        resultNumber {
+            let success, result = Decimal.TryParse str
+            if success then return result
+        }
