19f7f1f19a7ef50770ac26a9fbfc9b5ddbd78767
Focus <maniaclord@bk.ru>
2021-11-30 22:14:24 +0300

    person web app mvc

---
+++ HW7/WebAppMVC/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Controllers
+{
+    [Route("Person")]
+    public class PersonController : Controller
+    {
+        [HttpGet]
+        public IActionResult Person() =>
+            View();
+
+        [HttpPost]
+        public IActionResult Person(Person person) =>
+            View(person);
+    }
+}
