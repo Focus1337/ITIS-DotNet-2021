448904268ea79b4e885f746016ef02ddda1f8cad
Focus <maniaclord@bk.ru>
2021-12-22 20:18:32 +0300

    giraffe project from HW6 added

--- HW12/Benchmark/Ebenchmark.cs
+++ HW12/Benchmark/Ebenchmark.cs
@@ -10,20 +10,54 @@ namespace Benchmark
     [MinColumn, MaxColumn]
     public class Ebenchmark
     {
-        private HttpClient _clientCs;
-        private HttpClient _clientFs;
+        private readonly HttpClient _clientCs;
+        private readonly HttpClient _clientFs;
 
         public Ebenchmark()
         {
             _clientCs = new WebApplicationFactory<Startup>().CreateClient();
+            _clientFs = new WebApplicationFactory<Startup>().CreateClient();
         }
 
-        private const string ResponseBody = "https://localhost:5001/calculate?expressionString=";
+        //private const string ResponseBody = "https://localhost:5001/calculate?expressionString=2+3";
+
+        // private static async Task Sum(HttpClient client) =>
+        //     await client.GetAsync("https://localhost:5001/calculate?expressionString=2+3"); //($"{ResponseBody}");
+        //
+        // [Benchmark]
+        // public async Task SumCs() => await Sum(_clientCs);
+        // // await _clientCs.GetAsync($"{ResponseBody}");
+        //
+        // [Benchmark]
+        // public async Task SumFs() => await Sum(_clientFs);
+        // //await _clientFs.GetAsync($"{ResponseBody}");
+        //
+        // private static async Task SomeExpression(HttpClient client) =>
+        //     await client.GetAsync("https://localhost:5001/calculate?expressionString=(2+3)/12*7+8*9");
+        //
+        // [Benchmark]
+        // public async Task ExpressionCs() => await SomeExpression(_clientCs);
+        //
+        // [Benchmark]
+        // public async Task ExpressionFs() => await SomeExpression(_clientFs);
+
+
+        private static void Sum(HttpClient client) =>
+            client.GetAsync("https://localhost:5001/calculate?expressionString=2+3"); //($"{ResponseBody}");
 
         [Benchmark]
-        public async Task SumCSharp()
-        {
-            var response = await _clientCs.GetAsync($"{ResponseBody}2+3");
-        }
+        public void SumCs() => Sum(_clientCs);
+
+        [Benchmark]
+        public void SumFs() => Sum(_clientFs);
+
+        private static void SomeExpression(HttpClient client) =>
+            client.GetAsync("https://localhost:5001/calculate?expressionString=(2+3)/12*7+8*9");
+
+        [Benchmark]
+        public void ExpressionCs() => SomeExpression(_clientCs);
+
+        [Benchmark]
+        public void ExpressionFs() => SomeExpression(_clientFs);
     }
 }
--- HW12/WebAppHW12/Repository/ComputedExpression.cs
+++ HW12/WebAppHW12/Repository/ComputedExpression.cs
@@ -7,7 +7,6 @@ namespace WebAppHW12.Repository
 // ReSharper disable once ClassNeverInstantiated.Global
     public class ComputedExpression
     {
-        public int Id { get; set; }
         public decimal V1 { get; init; }
         public decimal V2 { get; init; }
         public Operation Op { get; init; }
